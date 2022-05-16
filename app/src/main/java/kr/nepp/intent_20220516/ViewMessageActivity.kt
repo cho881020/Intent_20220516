acdbdea408286fbcad7d337fe25b0c17e79787b4
조경진 <cho881020@gmail.com>
2022-05-16 17:09:45 +0900

    Intent (2) - 이동한 화면에서 데이터 받아서 활용

--- app/src/main/java/kr/nepp/intent_20220516/ViewMessageActivity.kt
+++ app/src/main/java/kr/nepp/intent_20220516/ViewMessageActivity.kt
@@ -2,10 +2,22 @@ package kr.nepp.intent_20220516
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+//        이벤트 처리 X, 화면에 들어오자마자 => 데이터수신 + 텍스트뷰에 반영.
+
+//        1. 보내준 문구가 뭔지 변수에 저장.
+
+        val receivedMessage =  intent.getStringExtra("message")
+
+//        2. 저장한 문구를 텍스트뷰에 반영
+
+        txtMessage.text = receivedMessage
+
     }
 }
